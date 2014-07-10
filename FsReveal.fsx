a8810f26843e82eed09fd8a6fb3c0ce716c42895
Karlkim Suwamongkol <karlkim@outlook.com>
2014-07-10 00:06:23 -0500

    Get ready for F#Reveal Lightning Talk at CodeStock

--- FsReveal.fsx
+++ FsReveal.fsx
@@ -40,9 +40,21 @@
 ### FSharp.Formatting
 
 - F# tools for generating documentation (Markdown processor and F# code formatter).
-- It parses a literate F# script file (markdown & F# code) and generate HTML or PDF.
-- Code syntax highlighting support.
-- It also evaluates your F# code and produce tooltips.
+  - [Literate F# script file](http://tpetricek.github.io/FSharp.Formatting/sidescript.html) (markdown & F# code in fsx file)
+  - [Literate Markdown file](http://tpetricek.github.io/FSharp.Formatting/sidemarkdown.html) (markdown & F# code in md file)
+  - Generate HTML, PDF, reveal.js, etc.
+  - Markdown extensions : LaTeX
+  - Code syntax highlighting support.
+
+*)
+(*** slide-end ***)
+(*** slide-start ***)
+(**
+### FSharp.Formatting
+
+- Evaluate F# code using [FSharp.Compiler.Service](http://fsharp.github.io/FSharp.Compiler.Service/)
+- [FsLab](https://github.com/tpetricek/FsLab): [sample](http://kimsk.github.io/Intro-to-Statistics/)
+- Used by many F# and non-F# opensource projects
 *)
 (*** slide-end ***)
 (*** slide-start ***)
@@ -74,21 +86,35 @@ Try hovering the F# code with your mouse..
 (*** slide-end ***)
 (*** slide-end ***)
 (*** slide-start ***)
+(*** slide-start ***)
 (**
 ## Why F#Reveal?
 
 - F# Type inference
-- Don't need to specify type (most of the time)
-- Less verbose and **easy** to write
-- Can be **hard** to read without help from an IDE
+  - Don't need to specify type (most of the time)
+  - Less verbose and **easy** to write
+  - Can be **hard** to read without help from an IDE
 *)
+(*** slide-end ***)
+(*** slide-start ***)
+(**
+## Why F#Reveal?
+
+- Create your slides using markdown syntax
+- Syntax highlighting for many programming languages (not only F#)
+- LaTeX
+- F# code is evaluated
+  - Display values
+  - Charting and so on
+*)
+(*** slide-end ***)
 (*** slide-end ***)
 (*** slide-start ***)
 (*** slide-start ***)
 (** 
 ## Card Game Sample
 
-> Domain Driven Design with F# type System by *Scott Wlaschin*
+> [Domain Driven Design with F# type System](http://www.slideshare.net/ScottWlaschin/ddd-with-fsharptypesystemlondonndc2013) by *Scott Wlaschin*
 *)
 (*** slide-end ***)
 (*** slide-start ***)
@@ -141,13 +167,24 @@ Need to think like a compiler to understand the code above..
 ### JavaScript
  
     [lang=js]
-    var sum = function() {
-        var i, x = 0;
-        for (i = 0; i < arguments.length; ++i) {
-            x += arguments[i];
-        }
-        return x;
+    function copyWithEvaluation(iElem, elem) {
+        return function (obj) {
+            var newObj = {};
+            for (var p in obj) {
+                var v = obj[p];
+                if (typeof v === "function") {
+                    v = v(iElem, elem);
+                }
+                newObj[p] = v;
+            }
+            if (!newObj.exactTiming) {
+                newObj.delay += exports._libraryDelay;
+            }
+            return newObj;
+        };
     }
+    
+*code from [winjs](https://github.com/winjs/winjs/blob/master/src/js/WinJS/Animations/_TransitionAnimation.js) project*
 *)
 (*** slide-end ***)
 (*** slide-start ***)
@@ -155,8 +192,17 @@ Need to think like a compiler to understand the code above..
 ### Haskell
  
     [lang=haskell]
-    factorial 0 = 1
-    factorial n | n > 0 = n * factorial (n - 1)
+    recur_count k = 1 : 1 : zipWith recurAdd (recur_count k) (tail (recur_count k))
+            where recurAdd x y = k * x + y
+
+    main = do
+      argv <- getArgs
+      inputFile <- openFile (head argv) ReadMode
+      line <- hGetLine inputFile
+      let [n,k] = map read (words line)
+      printf "%d\n" ((recur_count k) !! (n-1))
+
+*code from [NashFP/rosalind](https://github.com/NashFP/rosalind/blob/master/mark_wutka%2Bhaskell/FIB/fib_ziplist.hs)*
 *)
 (*** slide-end ***)
 (*** slide-start ***)
@@ -164,12 +210,12 @@ Need to think like a compiler to understand the code above..
 ### SQL
  
     [lang=sql]
-    SELECT Book.title AS Title, COUNT(*) AS Authors
-    FROM  Book
-    JOIN  Book_author
-    ON  Book.isbn = Book_author.isbn
-    GROUP BY Book.title;
- 
+    select * 
+    from 
+      (select 1 as Id union all select 2 union all select 3) as X 
+    where Id in (@Ids1, @Ids2, @Ids3)
+
+*sql from [Dapper](https://code.google.com/p/dapper-dot-net/)* 
 *)
 (*** slide-end ***)
 (*** slide-end ***)
@@ -237,6 +283,29 @@ $ \Pr(A|B)=\frac{\Pr(B|A)\Pr(A)}{\Pr(B|A)\Pr(A)+\Pr(B|\neg A)\Pr(\neg A)} $
 (*** slide-end ***)
 (*** slide-start ***)
 (**
-- [Here](https://github.com/kimsk/FsReveal/blob/master/FsReveal.fsx) is the literate F# script file generating this presentation.
+### The Reality of a Developer's Life 
+
+**When I show my boss that I've fixed a bug:**
+  
+![When I show my boss that I've fixed a bug](http://www.topito.com/wp-content/uploads/2013/01/code-07.gif)
+  
+**When your regular expression returns what you expect:**
+  
+![When your regular expression returns what you expect](http://www.topito.com/wp-content/uploads/2013/01/code-03.gif)
+  
+*from [The Reality of a Developer's Life - in GIFs, Of Course](http://server.dzone.com/articles/reality-developers-life-gifs)*
+*)
+(*** slide-end ***)
+(*** slide-start ***)
+(**
+## Thank you for coming!
+
+- [F# Logo Project](https://www.surveymonkey.com/s/FSharpLogo)
+- [The F# Software Foundation](http://fsharp.org/)
+- [Community for F#](http://c4fsharp.net/) 
+- [#fsharp](https://twitter.com/search?src=typd&q=%23fsharp) on twitter
+- [Nashville F# User Group](http://www.meetup.com/nashfsharp/) & [F# Thai](https://www.facebook.com/groups/425426147563091/)
+- [F#Reveal](https://github.com/kimsk/FsReveal) (POC) on GitHub
+
 *)
 (*** slide-end ***)
