20711a6dce8710d79308092e676fecb63b0bc930
Karlkim Suwamongkol <karlkim@outlook.com>
2014-07-14 16:43:28 -0500

    FsReveal slides in fsx and md

---
+++ presentations/FsReveal.fsx
@@ -0,0 +1,33 @@
+﻿(**
+- title : FsReveal 
+- description : Introduction to FsReveal
+- author : Karlkim Suwanmongkol
+- theme : dark
+- transition : default
+
+***
+
+### What is FsReveal?
+
+- Generates [reveal.js](http://lab.hakim.se/reveal-js/#/) presentation from [markdown](http://daringfireball.net/projects/markdown/)
+- Utilizes [FSharp.Formatting](https://github.com/tpetricek/FSharp.Formatting) for markdown parsing
+
+***
+
+### Reveal.js
+
+- A framework for easily creating beautiful presentations using HTML.  
+  
+
+> **Atwood's Law**: any application that can be written in JavaScript, will eventually be written in JavaScript.
+
+***
+
+### FSharp.Formatting
+
+- F# tools for generating documentation (Markdown processor and F# code formatter).
+- It parses a literate F# script file (markdown & F# code) and generate HTML or PDF.
+- Code syntax highlighting support.
+- It also evaluates your F# code and produce tooltips.
+
+*)
