b04c646ab2d07974fcf6042cf9d2813eff83e7c6
Karlkim Suwamongkol <contact@karlk.im>
2014-08-02 19:44:26 -0500

    update to use version 0.0.3-beta

--- TryFsReveal/Script.fsx
+++ TryFsReveal/Script.fsx
@@ -1,8 +1,9 @@
-﻿#I @"..\packages\FSharp.Formatting.2.4.18\lib\net40"
-#r "FSharp.Literate.dll"
-#I @"..\packages\FSharp.Compiler.Service.0.0.57\lib\net40"
+﻿#I @"..\packages\FSharp.Formatting.2.4.18\lib\net40\"
+#I @"..\packages\FSharp.Compiler.Service.0.0.57\lib\net40\"
 #r "FSharp.Compiler.Service.dll"
-#r @"..\packages\FsReveal.0.0.1-beta\lib\net40\FsReveal.dll"
+#r "FSharp.Literate.dll"
+#load @"..\packages\FsReveal.0.0.3-beta\fsreveal\fsreveal.fsx"
+
 open FsReveal
 
 let outDir = @"g:\output"
