313aa13bd8fc65865e941ac05110c31c0911d511
Karlkim Suwamongkol <contact@karlk.im>
2014-08-05 17:21:29 -0500

    Make it easier to use
    - Specify versions of FSharp.Formatting and FSharp.Compiler.Service in
    NuGet
    - Add FSharp.Formatting and FSharp.Compiler.Service references to
    FsReveal.fsx

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -6,3 +6,7 @@
 
 #### 0.0.3-beta - August 2 2014
 * Fix file location again.
+
+#### 0.0.4-beta - August 5 2014
+* Specify versions of FSharp.Formatting and FSharp.Compiler.Service in NuGet
+* Add FSharp.Formatting and FSharp.Compiler.Service references to FsReveal.fsx
--- src/FsReveal/FsReveal.fsx
+++ src/FsReveal/FsReveal.fsx
@@ -1,3 +1,7 @@
-﻿#r @"..\lib\net40\FsReveal.dll"
-
+﻿#I @"..\..\FSharp.Formatting.2.4.18\lib\net40\"
+#I @"..\..\FSharp.Compiler.Service.0.0.57\lib\net40\"
+#r "FSharp.Compiler.Service.dll"
+#r "FSharp.Literate.dll"
+#r @"..\lib\net40\FsReveal.dll"
+ 
 FsReveal.FsRevealHelper.Folder <- __SOURCE_DIRECTORY__
