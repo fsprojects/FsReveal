84f2b493ba305b8ffc7cc7cf8ab2b7e2d361d411
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-13 14:16:49 +0200

    Use Paket and FAKE to run FsReveal

---
+++ build.fsx
@@ -0,0 +1,27 @@
+#r @"packages/FAKE/tools/FakeLib.dll"
+
+#load @"packages\FsReveal\fsreveal\fsreveal.fsx"
+
+open FsReveal
+open Fake
+
+let outDir = "output"
+
+Target "Clean" (fun _ ->
+    CleanDirs [outDir]
+)
+
+Target "GenerateSlides" (fun _ ->
+    !! "slides/*.md"
+    |> Seq.iter (fun file -> 
+            let fi = fileInfo file
+            let outputFileName = fi.Name.Replace(fi.Extension,".html")
+            FsReveal.GenerateOutputFromMarkdownFile outDir outputFileName file)
+)
+
+Target "All" DoNothing
+
+"Clean"
+  ==> "GenerateSlides"
+  ==> "All"
+RunTargetOrDefault "All"
