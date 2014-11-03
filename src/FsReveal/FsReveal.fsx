48586e741a4890f781aeb43144582c5a2705ff63
Troy Kershaw <hello@troykershaw.com>
2014-11-04 09:52:06 +1100

    Updated paths to work on mono
    Thanks!

--- src/FsReveal/FsReveal.fsx
+++ src/FsReveal/FsReveal.fsx
@@ -1,8 +1,9 @@
-﻿#I @"..\..\FSharp.Formatting\lib\net40\"
-#I @"..\..\FSharp.Compiler.Service\lib\net40\"
+#I @"../../FSharp.Formatting/lib/net40/"
+#I @"../../FSharp.Compiler.Service/lib/net40/"
+#I @"../lib/net40/"
 #r "FSharp.Compiler.Service.dll"
 #r "FSharp.Literate.dll"
-#r @"..\lib\net40\FsReveal.dll"
+#r "FsReveal.dll"
 
 printfn "load FSharp.Formatting ..." 
 printfn "load FSharp.Compiler.Service ..."
