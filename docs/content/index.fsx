67c676d7bfd2a66cbcad02c38ba403ed1d7d52be
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-11-04 16:27:58 +0100

    Change the tutorial to reference TryFsReveal

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -12,27 +12,13 @@
       <pre>PM> Install-Package FsReveal</pre>
   </div>
 
-  If you want to get start quickly, just look at [Script.fsx](https://github.com/fsprojects/FsReveal/blob/TryFsReveal/TryFsReveal/Script.fsx) in [TryFsReveal project](https://github.com/fsprojects/FsReveal/tree/TryFsReveal).
+  If you want to get start quickly, just checkout the [TryFsReveal branch](https://github.com/fsprojects/FsReveal/tree/TryFsReveal).
   
-  Or follow the quick sample on how to generate reveal.js slides using FsReveal below.
-
-  First, create a new F# script file and load FsReveal.fsx.
-*)
-#load @"..\..\packages\FsReveal\fsreveal\fsreveal.fsx"
-open FsReveal
-
-(**
-  Then call either `FsReveal.generateOutputFromScriptFile` or `FsReveal.generateOutputFromMarkdownFile` functions.
-
-  `outDir` is your slides output folder. After the slides are generated, FsReveal also copy all files required to host your reveal.js slides there.
-*)
-let outDir = @"c:\output"
-
-(** input is an F# script file *)
-FsReveal.GenerateOutputFromScriptFile outDir "index.html" "input.fsx"
-
-(** input is a markdown file *)
-FsReveal.GenerateOutputFromMarkdownFile outDir "index.html"  "input.md"
+  Run `build.cmd KeepRunning` (or `build.sh KeepRunning` on mono) and you can start to edit the slides in the `slides` folder.
+  
+  Whenever a change is detected the slides are automatically regenerated and updated in the `output` folder.
+  Use your favorite webbrowser to view the slides.  
+   
 (** 
 Contributing and copyright
 --------------------------
