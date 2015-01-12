d7a58be427fe6ac96cfe3003aca54c170fdf5cc6
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-01-12 11:58:42 +0100

    Fix RazorEngine folder

--- src/FsReveal/FsReveal.fsx
+++ src/FsReveal/FsReveal.fsx
@@ -1,6 +1,5 @@
 #I @"../../FSharp.Formatting/lib/net40/"
 #I @"../../FSharp.Compiler.Service/lib/net40/"
-#I @"../../RazorEngine/lib/net40/"
 #I @"../lib/net40/"
 #r "FSharp.Compiler.Service.dll"
 #r "FSharp.Literate.dll"
