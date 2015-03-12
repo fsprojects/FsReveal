af0d17cc40d2103d6c3e2ad26b17e6ef7b1342cd
Tomas Petricek <tomas@tomasp.net>
2015-03-12 02:33:35 +0100

    Use optional parameters and allow speechifying FSI evaluator

--- src/FsReveal/FsReveal.fs
+++ src/FsReveal/FsReveal.fs
@@ -13,23 +13,13 @@ module FsRevealHelper =
     let mutable RevealJsFolder = __SOURCE_DIRECTORY__    
     let mutable TemplateFile = Path.Combine(__SOURCE_DIRECTORY__,"template.html")
 
-type FsReveal = 
-    static member GetPresentationFromScriptString(text : string) = 
-        normalizeLineBreaks(text).Split('\n') |> FsReveal.GetPresentationFromScriptLines
-    static member GetPresentationFromMarkdown(text : string) = 
-        normalizeLineBreaks(text).Split('\n') |> FsReveal.GetPresentationFromMarkdownLines
-    
-    static member GetPresentationFromScriptLines lines = 
-        let fsx = Formatting.preprocessing lines
-        let fsi = FsiEvaluator()
-        Literate.ParseScriptString(fsx, fsiEvaluator = fsi) |> getPresentation
-    
-    static member GetPresentationFromMarkdownLines lines = 
-        Formatting.preprocessing lines
-        |> Literate.ParseMarkdownString
-        |> getPresentation
-    
-    static member GenerateOutput outDir outFile presentation = 
+type FsReveal private() = 
+    static let defaultFileName (other:FileInfo) optInput = 
+        match optInput with 
+        | Some fn -> fn
+        | None -> other.Name.Replace(other.Extension, ".html")
+
+    static let generateOutput outDir outFile presentation = 
         if Directory.Exists outDir |> not then 
             Directory.CreateDirectory outDir |> ignore
             printfn "Creating %s.." outDir
@@ -59,25 +49,60 @@ type FsReveal =
     static let checkIfFileExistsAndRun file f = 
         if File.Exists file then f()
         else printfn "%s does not exist. Abort!" file
+          
+    /// Creates a presentation from an F# Script file specified as string. This also evaluates
+    /// all F# code snippets in the presentation and embeds the outputs. If you do not specify
+    /// a custom FSI evaluator, a new default one is created. See `GenerateFromFile` for more info.
+    static member GetPresentationFromScriptString(text : string, ?fsxFile, ?fsiEvaluator) = 
+        normalizeLineBreaks(text).Split('\n')
+        |> getPresentationFromScriptLines fsxFile fsiEvaluator
+
+    /// Creates a presentation from a Markdown source file specified as string. This also evaluates
+    /// all F# code snippets in the presentation and embeds the outputs. If you do not specify
+    /// a custom FSI evaluator, a new default one is created. See `GenerateFromFile` for more info.
+    static member GetPresentationFromMarkdown(text : string, ?mdFile, ?fsiEvaluator) = 
+        normalizeLineBreaks(text).Split('\n')
+        |> getPresentationFromMarkdownLines mdFile fsiEvaluator
     
-    static member GenerateOutputFromScriptFile outDir outFile fsxFile = 
-        FsReveal.checkIfFileExistsAndRun fsxFile (fun () -> 
+    /// Write the specified presentation to a specified file in the output directory.
+    /// (if a file name is not specified, the default `index.html` will be used)
+    static member GenerateOutput(presentation, outDir, ?outFile) = 
+        generateOutput outDir (defaultArg outFile "index.html") presentation
+            
+    /// Processes a presentation specified as an F# Script. This also evaluates all 
+    /// F# code snippets in the presentation and embeds the outputs. If you do not specify
+    /// a custom FSI evaluator, a new default one is created. See `GenerateFromFile` for more info.
+    static member GenerateOutputFromScriptFile(fsxFile, outDir, ?outFile, ?fsiEvaluator) = 
+        checkIfFileExistsAndRun fsxFile (fun () -> 
             fsxFile
             |> File.ReadAllLines
-            |> FsReveal.GetPresentationFromScriptLines
-            |> FsReveal.GenerateOutput outDir outFile)
+            |> getPresentationFromScriptLines (Some fsxFile) fsiEvaluator
+            |> generateOutput outDir (defaultFileName (FileInfo fsxFile) outFile))
     
-    static member GenerateOutputFromMarkdownFile outDir outFile mdFile = 
-        FsReveal.checkIfFileExistsAndRun mdFile (fun () -> 
+    /// Processes a presentation specified as a Markdown. This also evaluates all 
+    /// F# code snippets in the presentation and embeds the outputs. If you do not specify
+    /// a custom FSI evaluator, a new default one is created. See `GenerateFromFile` for more info.
+    static member GenerateOutputFromMarkdownFile(mdFile, outDir, ?outFile, ?fsiEvaluator) = 
+        checkIfFileExistsAndRun mdFile (fun () -> 
             mdFile
             |> File.ReadAllLines
-            |> FsReveal.GetPresentationFromMarkdownLines
-            |> FsReveal.GenerateOutput outDir outFile)
+            |> getPresentationFromMarkdownLines  (Some mdFile) fsiEvaluator 
+            |> generateOutput outDir (defaultFileName (FileInfo mdFile) outFile))
     
-    static member GenerateFromFile outDir fileName = 
+    /// Processes a presentation specified as an F# Script file or a Markdown document. 
+    /// (When the file name has extension other than `fsx` or `md`, nothing happens).
+    ///
+    /// The method evaluates F# code snippets in the presentation and embeds the outputs. You
+    /// can specify a custom `fsiEvaluator` to add formatting for custom values and handle errors
+    /// during the valuation.  
+    ///
+    /// ## Parameters
+    /// - `fsiEvaluator` - Custom evaluator (you can use this parameter to add custom formatting
+    ///   that turns values into HTML when embedding them into the presentation)
+    static member GenerateFromFile(fileName, outDir, ?outFile, ?fsiEvaluator) = 
         let file = FileInfo fileName
-        let outputFileName = file.Name.Replace(file.Extension, ".html")
+        let outputFileName = defaultFileName file outFile
         match file.Extension with
-        | ".md" -> FsReveal.GenerateOutputFromMarkdownFile outDir outputFileName file.FullName
-        | ".fsx" -> FsReveal.GenerateOutputFromScriptFile outDir outputFileName file.FullName
+        | ".md" -> FsReveal.GenerateOutputFromMarkdownFile(file.FullName, outDir, outputFileName, ?fsiEvaluator=fsiEvaluator)
+        | ".fsx" -> FsReveal.GenerateOutputFromScriptFile(file.FullName, outDir, outputFileName, ?fsiEvaluator=fsiEvaluator)
         | _ -> ()
