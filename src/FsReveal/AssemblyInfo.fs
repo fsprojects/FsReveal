b18d87852c3386ebf3f88bf3893b27ef7056a81f
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-06-14 17:52:17 +0200

    Bump version to 0.10.3

--- src/FsReveal/AssemblyInfo.fs
+++ src/FsReveal/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FsReveal")>]
 [<assembly: AssemblyProductAttribute("FsReveal")>]
 [<assembly: AssemblyDescriptionAttribute("FsReveal parses markdown or F# script files and generates reveal.js slides.")>]
-[<assembly: AssemblyVersionAttribute("0.10.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.10.2")>]
+[<assembly: AssemblyVersionAttribute("0.10.3")>]
+[<assembly: AssemblyFileVersionAttribute("0.10.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.10.2"
+    let [<Literal>] Version = "0.10.3"
