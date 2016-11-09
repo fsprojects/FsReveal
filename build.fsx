da6e89aa1464182fbea6a822b78ece29fc35344e
Henrik Feldt <henrik@haf.se>
2016-11-09 16:07:17 +0100

    Bumping deps

--- build.fsx
+++ build.fsx
@@ -39,11 +39,11 @@ Target "Clean" (fun _ ->
     CleanDirs [outDir]
 )
 
-let fsiEvaluator = 
+let fsiEvaluator =
     let evaluator = FSharp.Literate.FsiEvaluator()
-    evaluator.EvaluationFailed.Add(fun err -> 
+    evaluator.EvaluationFailed.Add(fun err ->
         traceImportant <| sprintf "Evaluating F# snippet failed:\n%s\nThe snippet evaluated:\n%s" err.StdErr err.Text )
-    evaluator 
+    evaluator
 
 let copyStylesheet() =
     try
@@ -57,15 +57,15 @@ let copyPics() =
     with
     | exn -> traceImportant <| sprintf "Could not copy picture: %s" exn.Message
 
-let generateFor (file:FileInfo) = 
+let generateFor (file:FileInfo) =
     try
         copyPics()
         let rec tryGenerate trials =
             try
                 FsReveal.GenerateFromFile(file.FullName, outDir, fsiEvaluator = fsiEvaluator)
-            with 
+            with
             | exn when trials > 0 -> tryGenerate (trials - 1)
-            | exn -> 
+            | exn ->
                 traceImportant <| sprintf "Could not generate slides for: %s" file.FullName
                 traceImportant exn.Message
 
@@ -92,8 +92,11 @@ let socketHandler (webSocket : WebSocket) =
     while true do
       let! refreshed =
         Control.Async.AwaitEvent(refreshEvent.Publish)
-        |> Suave.Sockets.SocketOp.ofAsync 
-      do! webSocket.send Text (ASCII.bytes "refreshed") true
+        |> Suave.Sockets.SocketOp.ofAsync
+      let seg =
+        let bs = ASCII.bytes "refreshed"
+        ByteSegment bs
+      do! webSocket.send Text seg true
   }
 
 let startWebServer () =
@@ -107,10 +110,10 @@ let startWebServer () =
 
     let port = findPort 8083
 
-    let serverConfig = 
+    let serverConfig =
         { defaultConfig with
            homeFolder = Some (FullName outDir)
-           bindings = [ HttpBinding.mkSimple HTTP "127.0.0.1" port ]
+           bindings = [ HttpBinding.createSimple HTTP "127.0.0.1" port ]
         }
     let app =
       choose [
@@ -131,7 +134,7 @@ Target "GenerateSlides" (fun _ ->
 
 Target "KeepRunning" (fun _ ->
     use watcher = !! (slidesDir + "/**/*.*") |> WatchChanges handleWatcherEvents
-    
+
     startWebServer ()
 
     traceImportant "Waiting for slide edits. Press any key to stop."
@@ -162,5 +165,5 @@ Target "ReleaseSlides" (fun _ ->
 
 "GenerateSlides"
   ==> "ReleaseSlides"
-  
+
 RunTargetOrDefault "KeepRunning"
