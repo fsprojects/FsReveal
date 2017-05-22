f195f38d49c0309e1e5a8fe5bfb997939db1e26d
Henrik Feldt <henrik@haf.se>
2017-05-22 10:33:00 +0200

    Newer nugets

--- build.fsx
+++ build.fsx
@@ -12,7 +12,7 @@
 let gitOwner = "myGitUser"
 let gitHome = "https://github.com/" + gitOwner
 // The name of the project on GitHub
-let gitProjectName = "MyProject"
+let gitProjectName = "FsReveal"
 // The name of the GitHub repo subdirectory to publish slides to
 let gitSubDir = ""
 
